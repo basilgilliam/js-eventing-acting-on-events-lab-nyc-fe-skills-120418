ffe79347aef5bde0c7a35ac8b87b6e67e56ca8df
Basil Gilliam <basilgilliam@gmail.com>
2019-01-30 01:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const moveDodgerLeft = () => {
   const dodger = document.getElementById('dodger')
   const currentLeft = dodger.style.left
-  const currentLeftNumber = currentLeft.replace('px', '')
+  let currentLeftNumber = currentLeft.replace('px', '')
+  currentLeftNumber = parseInt(currentLeftNumber, 10)
+  dodger.style.left = currentLeftNumber - 5 + 'px'
 } 
 
 const moveDodgerHelper = (event) => {
